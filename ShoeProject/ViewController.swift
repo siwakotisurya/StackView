6e99642ef1ba85ce9a53fa1ead939fedbdb338d3
Surya Siwakoti <suryasiwakoti@Suryas-MacBook-Pro.local>
2016-04-17 14:02:58 +0545

    project shoe generated for learning stack view

---
+++ ShoeProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShoeProject
+//
+//  Created by Surya Siwakoti on 4/17/16.
+//  Copyright © 2016 Surya Siwakoti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
